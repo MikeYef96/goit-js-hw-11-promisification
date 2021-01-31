a9f121c540b5484f80ff8d1fc5e19aaa12d2a347
MikeYef96 <mike.yef@gmail.com>
2021-01-31 15:43:36 +0300

    solved task-2 & task-3

--- src/js/task-2.js
+++ src/js/task-2.js
@@ -9,24 +9,17 @@ const users = [
   { name: 'Lux', active: false },
 ];
 
-const toggleUserState = (allUsers, userName, callback) => {
-  const updatedUsers = allUsers.map(user =>
-    user.name === userName ? { ...user, active: !user.active } : user,
-  );
-
-  callback(updatedUsers);
+const toggleUserState = (allUsers, userName) => {
+  return new Promise(resolve => {
+    const updatedUsers = allUsers.map(user =>
+      user.name === userName
+        ? resolve({ ...user, active: !user.active })
+        : user,
+    );
+  });
 };
 
 const logger = updatedUsers => console.table(updatedUsers);
 
-/*
- * Сейчас работает так
- */
-toggleUserState(users, 'Mango', logger);
-toggleUserState(users, 'Lux', logger);
-
-/*
- * Должно работать так
- */
 toggleUserState(users, 'Mango').then(logger);
 toggleUserState(users, 'Lux').then(logger);
